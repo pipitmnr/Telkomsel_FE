ff532c7d8ed10c4942e5dfcbe0414ef317b81ad0
pipitmnr <syafitri@alterra.id>
2020-05-05 11:48:01 +0700

    first commit

---
+++ src/route/mainRoute.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Provider } from "unistore/react";
+import { store } from "../store";
+
+const MainRoute = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          {/* PAGES ROUTING */}
+          {/* <Route exact path="/" component={Home} /> */}
+          {/* <Route exact path="/login" component={Login} /> */}
+          {/* <Route component={NotMatch} /> */}
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+export default MainRoute;
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import createStore from "unistore";
+
+const initialState = {
+};
+
+export const store = createStore(initialState);
+
+export const actions = store => ({
+  
+});
