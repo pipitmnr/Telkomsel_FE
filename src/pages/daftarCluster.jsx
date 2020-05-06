ec34055bdbbb336c079b2b6c5790b033d919275b
hamdiranu <hamdira@sci.ui.ac.id>
2020-05-06 15:25:24 +0700

    feature/page_title_admin -add table+pagination

--- src/pages/daftarCluster.jsx
+++ src/pages/daftarCluster.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'unistore/react';
-import { Button, Dropdown } from 'semantic-ui-react';
+import {
+	Button,
+	Dropdown,
+	Table,
+	Checkbox,
+	Pagination,
+} from 'semantic-ui-react';
 import { actions } from '../store';
 import '../styles/sidebar.css';
-import '../styles/titleAdmin.css';
+import '../styles/daftarDataCluster.css';
 import Sidebar from '../components/sidebar';
 import TitlePage from '../components/titleAdmin';
+
+const totalList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const optionsItem = [
 	{ key: 1, text: 'Semua Item', value: 'Semua Item' },
@@ -85,6 +93,53 @@ class DaftarCluster extends Component {
 											</li>
 										</ul>
 									</div>
+								</div>
+							</div>
+							<div className='container'>
+								<div
+									className='row tabel-data-cluster'
+									style={{ padding: '20px 30px' }}
+								>
+									<Table basic='very'>
+										<Table.Header>
+											<Table.Row className='thead-col'>
+												<Table.HeaderCell>
+													<Checkbox label={<label>No SKU</label>} />
+												</Table.HeaderCell>
+												<Table.HeaderCell>Nama Produk</Table.HeaderCell>
+												<Table.HeaderCell>Tipe Produk</Table.HeaderCell>
+												<Table.HeaderCell>Harga Produk</Table.HeaderCell>
+												<Table.HeaderCell>Sisa Stok</Table.HeaderCell>
+												<Table.HeaderCell>User</Table.HeaderCell>
+												<Table.HeaderCell></Table.HeaderCell>
+												<Table.HeaderCell></Table.HeaderCell>
+											</Table.Row>
+										</Table.Header>
+
+										<Table.Body>
+											{totalList.map((value, index) => {
+												return (
+													<Table.Row>
+														<Table.Cell>
+															<Checkbox label={<label>7881218218</label>} />
+														</Table.Cell>
+														<Table.Cell>Perdana Segel No Cantik</Table.Cell>
+														<Table.Cell>Perdana Segel</Table.Cell>
+														<Table.Cell>Rp 15.000</Table.Cell>
+														<Table.Cell>15</Table.Cell>
+														<Table.Cell>Aditya</Table.Cell>
+														<Table.Cell>:</Table.Cell>
+														<Table.Cell></Table.Cell>
+													</Table.Row>
+												);
+											})}
+										</Table.Body>
+									</Table>
+								</div>
+							</div>
+							<div className='container pagination-cluster'>
+								<div className='row'>
+									<Pagination defaultActivePage={5} totalPages={10} />
 								</div>
 							</div>
 						</div>
