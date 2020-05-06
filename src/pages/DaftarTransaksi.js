b0c33138e1696950dbe65db8d0dafb2634880d4d
Woka Aditama <woka@Wokas-MacBook-Air.local>
2020-05-06 10:56:59 +0700

    fixing filter error

--- src/pages/DaftarTransaksi.js
+++ src/pages/DaftarTransaksi.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Table, Button, Row, Col, Container} from "react-bootstrap"
-import Filter from "../components/Filter"
+import {Table, Navbar, DropdownButton, Dropdown,Button, Row, Col, Container} from "react-bootstrap"
+
 import './transaksi.css';
-import Sidebar from "../components/Sidebar"
+
 
 class Transactions extends React.Component{
     render(){
@@ -10,11 +10,29 @@ class Transactions extends React.Component{
     <Container fluid>
     <Row>
      <Col md="3" id="sidebar">
-      <Sidebar/>
+     
      </Col>
      <Col md= "8">
      <div id="transaksi">      
-       <Filter/>
+     <Navbar expand="lg" id="filter_one">
+                <Navbar.Collapse>     
+            <b>Filter</b> : &nbsp; <Dropdown>
+                <DropdownButton className="d-inline-block align-center" title="Semua Status">
+                    <Dropdown.Item href="">Terbayar</Dropdown.Item>
+                    <Dropdown.Item href="">Gagal</Dropdown.Item>
+                    <Dropdown.Item href="">Terkirim</Dropdown.Item>
+                    <Dropdown.Item href="">Menunggu Pembayaran</Dropdown.Item>
+                </DropdownButton>
+                &nbsp;
+                <DropdownButton className="d-inline-block align-center" title="Semua Kluster" variant="danger">
+                    <Dropdown.Item href="">Kluster 1</Dropdown.Item>
+                    <Dropdown.Item href="">Kluster 2</Dropdown.Item>
+                    <Dropdown.Item href="">Kluster 3</Dropdown.Item>
+                    <Dropdown.Item href="">Kluster 4</Dropdown.Item>
+                </DropdownButton>
+            </Dropdown>
+            </Navbar.Collapse>   
+            </Navbar>
        <Table striped bordered hover variant="white">
         <thead>
             <th>Nomor Pesanan</th>
