7519e858b11d31bb6062d64d61278a5ba0ddeda2
Garry Ariel <garry@alterra.id>
2020-05-06 10:22:59 +0700

    feature/checkbox_filter - template done

--- src/pages/shopping.jsx
+++ src/pages/shopping.jsx
@@ -3,7 +3,10 @@ import { withRouter } from "react-router-dom";
 import { connect } from "unistore/react";
 import { actions } from "../store";
 import Header from "../components/header";
-import ProductListWithImage from "../components/productListWithImage"
+import ProductListWithImage from "../components/productListWithImage";
+import CheckboxFilter from "../components/checkboxFilter";
+import "../styles/bootstrap.min.css";
+import "../styles/shopping.css";
 
 class Shopping extends Component {
   
@@ -16,8 +19,17 @@ class Shopping extends Component {
        <React.Fragment>
         <Header menuActive={this.props.location.pathname}/>
         <div className="header-white-space"></div>
-        <ProductListWithImage />
-        <ProductListWithImage />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-md-2 shopping-filter-container">
+              <CheckboxFilter />
+            </div>
+            <div className="col-12 col-md-10 shopping-product-list">
+              <ProductListWithImage />
+              <ProductListWithImage />
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
