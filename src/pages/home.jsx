2d54c5dc7d6132c922273761945487d207b48133
pipitmnr <syafitri@alterra.id>
2020-05-08 12:45:26 +0700

    add productlistwithimage to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,8 @@ import { connect } from 'unistore/react';
 import { actions } from '../store';
 import Header from '../components/header';
 import Footer from "../components/footer";
+import ProductListWithImage from '../components/productListWithImage';
+import '../styles/home.css';
 
 class Home extends Component {
   componentDidMount = async () => {};
@@ -13,8 +15,18 @@ class Home extends Component {
       <React.Fragment>
         <Header menuActive={this.props.location.pathname}/>
         <div className="header-white-space"></div>
-        <div className="container">
-          halo
+        <div className="container-fluid">
+          <div className="carousel">
+            <img src={require("../images/#dirumahterusbelajar.jpg")} alt=""/>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-md-12 product-details-key">
+                Produk Unggulan
+            </div>
+            <div className="col-md-12 mb-4">
+                <ProductListWithImage/>
+            </div>
+          </div>
         </div>
         <Footer />
       </React.Fragment>
