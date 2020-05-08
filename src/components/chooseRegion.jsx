7fbb6438a09cf4c2d756f73358a6cbdccadc73c4
Garry Ariel <garry@alterra.id>
2020-05-08 16:14:00 +0700

    feature/shopping_page - formatting region name

--- src/components/chooseRegion.jsx
+++ src/components/chooseRegion.jsx
@@ -6,12 +6,51 @@ import "../styles/bootstrap.min.css";
 import "../styles/chooseRegion.css";
 
 class ChooseRegion extends Component {
-  
+    /**
+     * The following method is used to transform regionName.json file into array
+     */
+    transformJson = () => {
+        // Get all regions in Indonesia in JSON format
+        let regionNameJson = require("../json/regionName.json");
+
+        // Transform the JSON format into desired format
+        let regionDetail = [];
+        for (var index in regionNameJson) {
+            let province = regionNameJson[index];
+            let provinceName = Object.keys(province)[0];
+            let cityList = province[provinceName];
+            for (var cityIndex in cityList) {
+                if (typeof cityList[cityIndex] === "object") {
+                    let city = cityList[cityIndex];
+                    let cityName = Object.keys(city)[0];
+                    let villageList = city[cityName];
+                    for (var villageIndex in villageList) {
+                        regionDetail.push(villageList[villageIndex] + ", " + cityName + ", " + provinceName)
+                    }
+                } else {
+                    regionDetail.push(cityList[cityIndex] + ", " + provinceName)
+                }
+            }
+        }
+
+        return regionDetail;
+    }
+    
     componentDidMount = async () => {
       
     };
   
     render() {
+        // Get all formatted region in Indonesia
+        let regionName = this.transformJson();
+
+        // Define JSX variable which will provide datalist options
+        let datalistOptions = regionName.map((region) => {
+            return (
+                <option value={region} />
+            )
+        })
+
         return (
             <React.Fragment>
                 <div className="choose-region-container">
@@ -22,7 +61,7 @@ class ChooseRegion extends Component {
                                 <label for="regionSearch"><span className="choose-region-label">Kota / Kecamatan</span></label>
                                 <input id="regionSearch" list="regionName" type="text" className="choose-region-input" placeholder="Contoh: Jawa Timur, Kota Surabaya, Asemrowo"/>
                                 <datalist id="regionName" className="choose-region-datalist">
-                                    <option value="Internet Explorer" />
+                                    {datalistOptions}
                                 </datalist>
                             </div>
                         </form>
