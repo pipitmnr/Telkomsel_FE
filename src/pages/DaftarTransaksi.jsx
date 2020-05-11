804f7fb1c6412e92aabdc4a958a467ee7bdd3ecc
Woka Aditama <woka@Wokas-MacBook-Air.local>
2020-05-11 14:23:45 +0700

    push fatal1

--- src/pages/DaftarTransaksi.jsx
+++ src/pages/DaftarTransaksi.jsx
@@ -30,26 +30,35 @@ class Transactions extends React.Component {
   };
   constructor(props) {
     super(props);
-    this.state = {list_transaksi: transaksi, direction:null, arrow:null};
+	this.state = {list_transaksi: transaksi, direction:null, nomor_pesanan:null,
+		nama_pemesan:null, alamat:null, detail_order:null, dummy:null};
   }
+    changePayment=(stats, ids)=>{
+		transaksi.map(el=>{
+			if(el.id===ids){
+			el.status=stats;
+			el.color="success"
+		this.setState({dummy:"new"})}
+		})
 
+	}
 	sortingOrder=(key)=>{
 		if(this.state.direction===null|| this.state.direction==='descending'){
-		transaksi.sort((a,b)=>{var newA=a[key].split('/').concat(),newB=b[key].split('/').concat();
+		transaksi.sort((a,b)=>{var newA=a[key].split(/[./,-]+/).concat(),newB=b[key].split('/').concat();
 		if (newA<newB){
 			return -1
 		}
 	    if (newA>newB){
 			return 0
 		}})
-		this.setState({list_transaksi:transaksi})
+		this.setState({list_transaksi:transaksi, direction:"ascending", key:"down"})
 	    }else if(this.state.direction==="ascending"){
-		transaksi.sort((a,b)=>{var newA=a[key].split('/').concat(),newB=b[key].split('/').concat();
+		transaksi.sort((a,b)=>{var newA=a[key].split(/[./,-]+/).concat(),newB=b[key].split('/').concat();
 		if (newA>newB){
 			return -1
 		}
 	    })
-		this.setState({list_transaksi:transaksi})
+		this.setState({list_transaksi:transaksi,direction:"descending", arrow:"down"})
 	}
 }
 	filterJenis=(stats)=>{
@@ -179,27 +188,46 @@ class Transactions extends React.Component {
 								<br/>
 								<Table  className="tabel-transaksi" striped bordered hover variant='white'>
 									<thead>
-										<th><Dropdown as={ButtonGroup}>
-											<DropdownButton
-												className='d-inline-block align-center'
-												title="Nomor Pemesanan"
+										<th><Dropdown drop={this.state.nomor_pemesanan}>
+											<Dropdown.Toggle
+												id='sort-transaction'
+												title="Nomor Pesanan"
 												variant="white"
+												onClick={event=>this.sortingOrder('nomor_pesanan')}
 											>
-												<Dropdown.Item
-													onClick={event=> this.sortingOrder('nomor_pesanan')}
-												>
-													Ascending
-												</Dropdown.Item>
-												<Dropdown.Item
-													onClick={event=> this.sortingOrder('nomor_pesanan')}
-												>
-													Descending
-												</Dropdown.Item>
-												</DropdownButton>
+												Nomor Pesanan
+												</Dropdown.Toggle>
 											</Dropdown></th>
-										<th>Nama Pemesan</th>
-										<th>Kota/Kecamatan Alamat</th>
-										<th>Detail Pesanan</th>
+										<th><Dropdown drop={this.state.nama_pemesan}>
+											<Dropdown.Toggle
+												id='sort-transaction'
+												title="Nama Pemesan"
+												variant="white"
+												onClick={event=>this.sortingOrder('nama_pemesan')}
+											>
+												Nama Pemesan
+												</Dropdown.Toggle>
+											</Dropdown></th>
+										<th><Dropdown drop={this.state.alamat}>
+											<Dropdown.Toggle
+												id='sort-transaction'
+												title="alamat"
+												variant="white"
+												onClick={event=>this.sortingOrder('alamat')}
+											>
+												Kota/Kecamatan Alamat
+												</Dropdown.Toggle>
+											</Dropdown></th>
+										<th><Dropdown drop={this.state.detail_order}>
+											<Dropdown.Toggle
+												id='sort-transaction'
+												title="detail order"
+												variant="white"
+												onClick={event=>this.sortingOrder('detail_order')}
+											>
+												Detail Pesanan
+												</Dropdown.Toggle>
+											</Dropdown></th>
 										<th>Status</th>
 									</thead>
 									<tbody>
@@ -211,8 +239,14 @@ class Transactions extends React.Component {
 												{row.alamat}
 											</td>
 									          <td>{row.detail_order}</td>
-											<td>
-												<Button variant={row.color}>{row.status}</Button>
+										
+											<td>{row.status==="Terbayar" ?
+											<Dropdown>
+												<DropdownButton variant={row.color} title={row.status}>
+													<Dropdown.Item onClick={event=>this.changePayment("Terkirim", row.id)}>Terkirim</Dropdown.Item>
+												</DropdownButton>
+											</Dropdown>: <Button variant={row.color}>{row.status}</Button>}
+									
 											</td>
 											
 										</tr>))}
